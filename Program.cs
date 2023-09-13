ad0e5cfbbb11b4c3cc35e5eb09900db1331d1afd
CATADRXL <Katharina.Draexl@tecalliance.net>
2023-09-13 09:22:19 +0200

    standard start setup füór C#

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Gebe mir deinen Namen!");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("hello, " + name);
+            
+        }
+    }
+}
