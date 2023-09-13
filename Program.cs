a708c9eb35864f0ab2541422ade66e5e81ba3d4d
CATADRXL <Katharina.Draexl@tecalliance.net>
2023-09-13 15:54:08 +0200

    88ü

--- Program.cs
+++ Program.cs
@@ -8,22 +8,13 @@ using System.Security.Cryptography.X509Certificates;
 namespace HelloWorld
 {
     class Program
-    {
-        static void Main(string[] args)
-
-        {
-
-            Console.WriteLine("Bis zu welcher Zahl soll aufsummiert werden? (0,1,2,...,x)");
-
+    { static void Main(string[] args)
+        {  Console.WriteLine("Bis zu welcher Zahl soll aufsummiert werden? (0,1,2,...,x)");
             int untilNumber = Convert.ToInt16(Console.ReadLine());
             int ergebnis = 0;
             for(int i = 0; i <=untilNumber;i++)
-            {
-                    ergebnis+= i;
-
-                    Console.WriteLine($"Added number {i}, current result {ergebnis}");
-            }
+            {   ergebnis+= i;
+                    Console.WriteLine($"Added number {i}, current result {ergebnis}");}
         }
-
 }
 }
