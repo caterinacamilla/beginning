d48ae459307bcbb42a7e8c77dab0f6b5ba15ce3c
CATADRXL <Katharina.Draexl@tecalliance.net>
2023-09-14 10:41:44 +0200

    aufgabe 7

--- Program.cs
+++ Program.cs
@@ -9,15 +9,8 @@ namespace HelloWorld
 {
     class Program
     { static void Main(string[] args)
-        { int Ergebnis = 1+1;
-        Console.WriteLine(Ergebnis);
-        Console.WriteLine("Höchstgrenze:");
-int Max = Convert.ToInt32(Console.ReadLine());
-long Fakultaet = 1; //Vorbelegung mit 1 ist wichtig!!!
-for (long i = 1; i <= Max; i++)
-{
-Fakultaet *= i;
-}
-Console.WriteLine("{0}! = {1}", Max, Fakultaet);
+        { Console.WriteLine("Schreibe etwas!");
+            string mytext = Console.ReadLine();
+            string mytext2 = mytext.Replace("g","u").Replace("G","U");
 }
 }}
