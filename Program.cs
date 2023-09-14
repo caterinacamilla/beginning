14ed3e091bbd876bf88a776e6c9f5486f90512ab
CATADRXL <Katharina.Draexl@tecalliance.net>
2023-09-14 15:12:59 +0200

    12

--- Program.cs
+++ Program.cs
@@ -8,9 +8,17 @@ using System.Security.Cryptography.X509Certificates;
 namespace HelloWorld
 {
     class Program
-    { static void Main(string[] args)
-        { Console.WriteLine("Schreibe etwas!");
-            string mytext = Console.ReadLine();
-            string mytext2 = mytext.Replace("g","u").Replace("G","U");
+    { 
+        static void Main(string[] args)
+        { 
+            List<string> names = new List<string> {"Luca", "Katharina", "Hans", "Marcia", "Benny", "Lea"};
+            foreach(string name in names)
+            {
+                if(name.Contains('e'))
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
 }
-}}
