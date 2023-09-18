94f0b3421ae23d3a41a9161028d8db7e7504d47d
CATADRXL <Katharina.Draexl@tecalliance.net>
2023-09-18 15:54:42 +0200

    while schleife

--- Program.cs
+++ Program.cs
@@ -15,6 +15,20 @@ namespace HelloWorld
            //Berechne die Fibonacci Zahlen bis zu einer eingegebenen Zahl vm Nutzer
            //1+1 =2
            //1+2 =3
+
+            Console.WriteLine("Bis zu welcher Zahl sollen die Fibonacci Zahlen ausgegeben werden?");
+            int eingabe = Convert.ToInt32(Console.ReadLine());
+            int fib1 = 1;
+            int fib2 = 1;
+            int ergebnis = 0;
+
+            while(eingabe > (fib1+fib2))
+            {
+                ergebnis = fib1 + fib2;
+                Console.WriteLine($"Ergebnis {fib1} + {fib2} = {ergebnis}");
+                fib1 = fib2;
+                fib2 = ergebnis;
+            }
     }
 }
 }
