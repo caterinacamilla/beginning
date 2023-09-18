e243695ebe617f6f9a4e5b4c6815dc095cb28f72
CATADRXL <Katharina.Draexl@tecalliance.net>
2023-09-18 15:07:01 +0200

    zahlen rückwärts aufzählen

--- Program.cs
+++ Program.cs
@@ -11,90 +11,17 @@ namespace HelloWorld
     { 
         static void Main(string[] args)
         {
-            //Wiederholung
-            //Initialisiere 2 Zahlen und gebe das Ergebnis der Addition auf der Console aus
-            //"Das Ergebnis von zahl1 und zahl2 ist XYZ"
+          
+           //Frage den Nutzer nach einer Zahl und gebe die Zahlen rückwärts aus
+          //p.e. 65 => 64, 63, 62, 61, ...
 
-            int zahl1 = 1;
-            int zahl2 = 2;
-            int ergebnis = zahl1 + zahl2;
-            Console.WriteLine ($"Das ERgebnis von {zahl1} und {zahl2} ist {ergebnis}");
-
-            //Wiederholung
-            //Lasse vom Nutzer einen Text eingeben und gebe folgende Punkte aus
-            //Länge des Textes
-            //Anzahl wie oft Buchstabe h auftaucht
-
-            Console.WriteLine ("Gebe mir einen Text mit viele h indem");
-            string mytext = Console.ReadLine();
-            Console.WriteLine (mytext.Length);
-            mytext.Split('H');
-            Console.WriteLine (mytext.Split('H').Length);
-
-            
-            //Wiederholung
-            //Lasse vom Nutzer einen Text eingeben und gebe folgende Punkte aus
-            //- Länge des Textes
-            //- Anzahl  wie oft Buchstabe h auftaucht            
-
-            Console.WriteLine("Gebe mir einen Text");
-            string eingegebenerText = Console.ReadLine();
-            int laengetext = eingegebenerText.Length;
-            Console.WriteLine("Die Länge des Textes ist " + laengetext);
-            
-            //Möglichkeit 1
-            //string[] splittedText = eingegebenerText.Split("h");
-            
-            //Möglichkeit 2
-            int count = 0;
-            for(int i = 0;i< eingegebenerText.Length;i++)
-            {
-                if(eingegebenerText[i] == 'h')
-                {
-                    count++;
-                }
-            }
-            Console.WriteLine($"Es gibt {count} h's in deinem Text.");
-           //Möglichkeit 2b
-           foreach(char c in eingegebenerText)
-           {
-                if(c == 'h')
-                {
-                    count++;
-                }
-           }
-
-           //Möglichkeit
-           int numberofh = eingegebenerText.Count(c => c == 'h');
-           Console.WriteLine($"Es gibt {numberofh} h's in deinem Text");
-
-            //Frage den Nutzer wieviele Zahlen er in einem Array ingeben möchte
-            //und lasse ihn anschließend mit Zahlen befüllen
-            //Gebe anschließend die Größte und die Kleinste Zahl und den Durchschnitt aus.
-
-            Console.WriteLine ("Gebe mir die gewünschte Größe deines Arrays an");
-            string eingabe = Console.ReadLine ();
-            int groese = Convert.ToInt16(eingabe);
-            int [] myArray = new int [groese];
-        
-            Console.WriteLine("Wie groß soll das Array sein?");
-
-          int aa = Convert.ToInt32(Console.ReadLine());
-
-          int[] myarray = new int[aa];
-            for(int i=0; i<aa; i++)
-
-              {
-            int zahl = Convert.ToInt32(Console.ReadLine());
-            myarray[i] = zahl;
-
-
-            Console.WriteLine("------------------");    
-            Console.WriteLine($"Die kleinste Zahl ist: {myarray.Min()}");    
-            Console.WriteLine($"Die groesste Zahl ist: {myarray.Max()}");
-            Console.WriteLine($"Der Durchschnittswert beträgt: {myarray.Average()}");
+            Console.WriteLine("Gib eine Zahl ein");
+            Console.WriteLine("------------------");
+          for(int zahl = Convert.ToInt32(Console.ReadLine()); zahl>=0; zahl--)
+          {
+            Console.WriteLine(zahl);
+          }
         
     }
-}
 }
 }
